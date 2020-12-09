625ba1191eb4579cd79385aa7ba2128234ddc1e5
Ashleigh Udoh <mail@audoh.co.uk>
2020-12-09 13:47:52 +0000

    improved prompt error handling

--- Modules/PowerShellUtilities.Core/Prompt.ps1
+++ Modules/PowerShellUtilities.Core/Prompt.ps1
@@ -1,22 +1,30 @@
 function Prompt {
-  $warningPreference = "SilentlyContinue";
+  $WarningPreference = "SilentlyContinue";
 
-  $date = "[$(Get-Date -Format HH:mm)]";
-  $path = "$(Get-ShortPath)";
-  $git = "";
+  try {
+    $date = "[$(Get-Date -Format HH:mm)]";
+    $path = "$(Get-ShortPath)";
+    $git = "";
+    $color = "Black"
 
-  if (Get-IsGitRepository) {
-    $branch = Get-GitBranch;
-    $color = "Cyan";
+    if (Get-IsGitRepository) {
+      $branch = Get-GitBranch;
+      $color = "Cyan";
 
-    if (Get-HasUncommittedChanges) {
-      $color = "Red";
+      if (Get-HasUncommittedChanges) {
+        $color = "Red";
+      }
+      elseif (Get-HasUnpushedCommits $branch) {
+        $color = "Yellow";
+      }
+
+      $git = "($($branch))";
     }
-    elseif (Get-HasUnpushedCommits $branch) {
-      $color = "Yellow";
-    }
-
-    $git = "($($branch))";
+  }
+  catch {
+    Write-Host $_ -ForegroundColor "Red"
+    $git = "[error]"
+    $color = "Red"
   }
 
   [Console]::ResetColor()
