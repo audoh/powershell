a4c98c3c4f8daf63787bf5b6bb94ff143b4c139e
Ashleigh Udoh <mail@audoh.co.uk>
2020-12-09 13:48:17 +0000

    fixed index into null array error for new git branches with no origin

--- Modules/PowerShellUtilities.Git/Get-HasUnpushedCommits.ps1
+++ Modules/PowerShellUtilities.Git/Get-HasUnpushedCommits.ps1
@@ -2,7 +2,7 @@ function Get-HasUnpushedCommits {
   param ([Parameter(Position = 1)] [string] $branch = "$(Get-GitBranch)");
 
   $diff = Invoke-Expression "git rev-list --left-only --count $($branch)...origin/$($branch)";
-  if ($diff[0] -eq "0") {
+  if ($diff -and $diff[0] -eq "0") {
     return $False;
   }
   else {
