1d59f324fa51989401cdbbbe85a0c1d2d8b7e825
Ashleigh Udoh <mail@audoh.co.uk>
2021-02-10 00:00:32 +0000

    Added -Prune switch to Clear-UntrackedBranches

--- Modules/PowerShellUtilities.Git/Clear-UntrackedBranches.ps1
+++ Modules/PowerShellUtilities.Git/Clear-UntrackedBranches.ps1
@@ -1,4 +1,20 @@
+<#
+.SYNOPSIS
+  Clears out local Git branches whose tracking branches are gone.
+#>
 function Clear-UntrackedBranches {
+  [CmdletBinding()]
+  param (
+    [Parameter()]
+    [switch]
+    # If set, prunes local remote branches first .Parameter
+    $Prune
+  )
+
+  if ($Prune) {
+    git remote | ForEach-Object { git remote prune $_ }
+  }
+
   git branch --format "%(refname:short) %(upstream:track)" | ForEach-Object {
     $split = $_.Split(" ")
     if ($split[1] -eq "[gone]") {
